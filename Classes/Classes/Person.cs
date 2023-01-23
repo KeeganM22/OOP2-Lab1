954cc65b402b844e1de9c18ff03b9ed8f87408f1
KeeganM22 <119663094+KeeganM22@users.noreply.github.com>
2023-01-23 15:16:59 -0700

    Added correct files

---
+++ Classes/Classes/Person.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    internal class Person
+    {
+        private int id;
+        private string firstName;
+        private string lastName;
+        private string favoriteColor;
+        private int age;
+        private bool isWorking;
+
+        public string DisplayPersonInfo
+        {
+            get
+            {
+                string name =this.id + ": " + this.firstName+ " " + this.lastName + "'s favorate color is " + this.favoriteColor;
+               
+                return name;
+            }
+        }
+        public string ChangeFavoriteColor
+        {
+            get
+            {
+                this.favoriteColor = "white";
+                return this.favoriteColor;
+            }
+            set
+            {
+                this.favoriteColor = "white";
+            }
+            
+        }
+        public int GetAgeInTenYears
+        {
+            get
+            {
+                int newAge = this.age + 10;
+                
+                return newAge;
+            }
+        }
+        public string ToString
+        {
+            get
+            {
+                string formated = "ID: " + this.id + " \n Name: " +  this.firstName + " " + this.lastName + " \n Age: " + this.age + " \n Favorite Color: " + this.favoriteColor + "\n Working Status: "+ this.isWorking;
+                
+                return formated;
+
+            }
+        }
+
+        public Person(int id, string firstName, string lastName, string favoriteColor, int age, bool isWorking)
+        {
+            this.id = id;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.favoriteColor = favoriteColor;
+            this.age = age;
+            this.isWorking = isWorking;
+        }
+
+    }
+}
---
+++ Classes/Classes/Program.cs
@@ -0,0 +1,32 @@
+﻿using Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world!");
+            Person person1;
+            person1 = new Person(1, "Ian", "Brooks", "Red", 30, true);
+            Person person2;
+            person2 = new Person(2, "Gina", "James", "Green", 18, false);
+            Person person3;
+            person3 = new Person(3, "Mike", "Briscoe", "Blue", 45, true);
+            Person person4;
+            person4 = new Person(4, "mary", "Meals", "yellow", 28, true);
+
+            Console.WriteLine(person2.DisplayPersonInfo);
+            Console.WriteLine(person3.ToString);
+            Console.WriteLine(person1.ChangeFavoriteColor);
+            Console.WriteLine(person1.DisplayPersonInfo);
+            Console.WriteLine(person4.GetAgeInTenYears);
+
+        }
+    }
+}
